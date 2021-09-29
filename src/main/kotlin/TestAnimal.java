2ccdcb67cf7c2dc8ee97beca379b984b60a81729
VeeraNagendraBabu <65601804+VeeraNagendraBabu@users.noreply.github.com>
2021-09-29 17:27:26 +0530

    Update TestAnimal.java

--- src/main/kotlin/TestAnimal.java
+++ src/main/kotlin/TestAnimal.java
@@ -2,6 +2,7 @@ import java.util.logging.ConsoleHandler;
 
 public class TestAnimal {
     TestAnimal(String name){
+        
         System.out.println("Hi Dear, my name is "+ name);
     }
     private int numberOfleg=4;
